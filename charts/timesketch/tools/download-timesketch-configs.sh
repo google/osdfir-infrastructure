d7a6244b13d3b553740c6cb68d4fe3b68770e85c
wajihyassine <wajihyassine@gmail.com>
2023-11-03 13:27:00 -0700

    Clone repo and copy out configs instead

--- charts/timesketch/tools/download-timesketch-configs.sh
+++ charts/timesketch/tools/download-timesketch-configs.sh
@@ -9,20 +9,8 @@ set -e
 mkdir -p configs/
 
 echo -n "* Fetching configuration files.." 
-GITHUB_BASE_URL="https://raw.githubusercontent.com/google/timesketch/master"
 # Fetch default Timesketch config files
-wget $GITHUB_BASE_URL/data/timesketch.conf -O configs/timesketch.conf
-wget $GITHUB_BASE_URL/data/tags.yaml -O configs/tags.yaml
-wget $GITHUB_BASE_URL/data/plaso.mappings -O configs/plaso.mappings
-wget $GITHUB_BASE_URL/data/plaso_formatters.yaml -O configs/plaso_formatters.yaml
-wget $GITHUB_BASE_URL/data/generic.mappings -O configs/generic.mappings
-wget $GITHUB_BASE_URL/data/features.yaml -O configs/features.yaml
-wget $GITHUB_BASE_URL/data/ontology.yaml -O configs/ontology.yaml
-wget $GITHUB_BASE_URL/data/intelligence_tag_metadata.yaml -O configs/intelligence_tag_metadata.yaml
-wget $GITHUB_BASE_URL/data/sigma_config.yaml -O configs/sigma_config.yaml
-wget $GITHUB_BASE_URL/data/sigma/rules/lnx_susp_zmap.yml -O configs/lnx_susp_zmap.yml
-wget $GITHUB_BASE_URL/data/context_links.yaml -O configs/context_links.yaml
-wget $GITHUB_BASE_URL/data/scenarios/facets.yaml -O configs/facets.yaml
-wget $GITHUB_BASE_URL/data/scenarios/questions.yaml -O configs/questions.yaml
-wget $GITHUB_BASE_URL/data/scenarios/scenarios.yaml -O configs/scenarios.yaml
+git clone https://github.com/google/timesketch.git
+cp -r timesketch/data/* configs
+rm -rf timesketch
 echo "OK"
