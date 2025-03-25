9c2f7a6fe75745cce9a2342213817b6705bed0c6
Dan <daschwanden@google.com>
2025-03-25 13:51:10 +0000

    Streamlines exe sign cert

--- charts/osdfir-infrastructure/charts/grr/createSigningKeys.sh
+++ charts/osdfir-infrastructure/charts/grr/createSigningKeys.sh
@@ -9,13 +9,3 @@ openssl genrsa -out ./certs/exe-sign-private-key.pem
 
 # Generate the GRR client executable signing public key
 openssl rsa -in ./certs/exe-sign-private-key.pem -pubout -out ./certs/exe-sign-public-key.pem
-
-PRI_KEY=$(cat ./certs/exe-sign-private-key.pem | sed ':a;N;$!ba;s/\n/\\\\n/g')
-
-PUB_KEY=$(cat ./certs/exe-sign-public-key.pem | sed ':a;N;$!ba;s/\n/\\\\n/g')
-
-echo $PRI_KEY
-echo $PUB_KEY
-
-sed -i "s'EXE_SIGN_PRIVATE_KEY'$PRI_KEY'g" ./templates/server-local-secret.yaml
-sed -i "s'EXE_SIGN_PUBLIC_KEY'$PUB_KEY'g" ./templates/server-local-secret.yaml
